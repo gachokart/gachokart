65a808eaacba1404e411db7b8e4ec908c1efcf6f
gachokart <gachokwork@gmail.com>
2025-10-23 12:47:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 // Проксі для історії матчів
 app.get("/api/matches", async (req, res) => {
   const { account_id, count } = req.query;
-  const url = `https://api.opendota.com/api/players/${account_id}/matches?limit=${count || 20}`;
+  const url = `https://api.opendota.com/api/players/${account_id}/matches?limit=${count || 20}&project=lane_role&project=kills&project=deaths&project=assists&project=hero_id&project=duration&project=radiant_win&project=player_slot`;
   try {
     const response = await fetch(url);
     const data = await response.json();
