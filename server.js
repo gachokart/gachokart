a6b2009c56a60cb58cd6b365a709ce700891ba1f
gachokart <gachokwork@gmail.com>
2025-11-21 14:57:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,4 @@
-// server.js
-const express = require("express");
-const app = express();
-const PORT = 3000;
-
-// віддавати статичні файли (наприклад game1.html)
-app.use(express.static(__dirname));
-
-// тестовий маршрут
-app.get("/api/predict", (req, res) => {
-  res.json({ message: "Привіт, маршрут працює!" });
-});
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Сервер запущено: http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
